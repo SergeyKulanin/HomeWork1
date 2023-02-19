8c0234049bcbc631b7351e4c4606100e26e33b3b
Sergey Kulanin <sjulanin1981@gmail.com>
2023-02-19 09:23:17 +0300

    Максимальное из 3 чисел

---
+++ Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.Write("Введите A: ");
+int number1=int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int number2=int.Parse(Console.ReadLine());
+Console.Write("Введите C: ");
+int number3=int.Parse(Console.ReadLine());
+
+int max=number1;
+
+if(number2>max) max = number2;
+if(number3>max) max = number3;
+
+  Console.Write("max = ");  
+  Console.WriteLine(max);
