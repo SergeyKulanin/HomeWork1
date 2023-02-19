074b3ca8b9c930088937a1b1f517a2f983b13fd8
Sergey Kulanin <sjulanin1981@gmail.com>
2023-02-19 10:33:44 +0300

    Вывод четных чисел из диапазона

---
+++ Ex8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Clear();
+int i=1;
+Console.Write("Введите число: ");
+int N=int.Parse(Console.ReadLine());
+
+while(i <= N);
+{
+    if(i % 2 == 0);
+    {
+        Console.WriteLine($"{i} ");
+        i=i+1;
+    }
+    else
+    {
+        i=i+1;
+    }
+}
