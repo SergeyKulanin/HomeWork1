d4082a796c740f1e4d59d6f47739220e0a51700d
Sergey Kulanin <sjulanin1981@gmail.com>
2023-02-19 08:35:53 +0300

    Нахождение большего числа

---
+++ Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Введите A: ");
+int number1=int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int number2=int.Parse(Console.ReadLine());
+
+if(number1>number2)
+{
+  Console.WriteLine("Большим числом является А");  
+}
+else
+{
+  Console.WriteLine("Большим числом является B");
+}
